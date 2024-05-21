9e98cf1aad8e8a5c444993d92def28f167e2c513
James Nogueira <j.l.nogueira@tcu.edu>
2024-05-20 21:23:18 -0400

    Bishop Movement working

--- Brain/Movement/bishopMovement.cpp
+++ Brain/Movement/bishopMovement.cpp
@@ -20,8 +20,6 @@ void print_board(uint64_t num){
     }
 }
 
-
-
 std::tuple<uint64_t, uint64_t> calculateMainDiag(uint64_t piece){
     if(piece & 0x0100000000000000ULL)
         return std::make_tuple(0x0100000000000000ULL, 0x0100000000000000ULL);
@@ -59,33 +57,65 @@ std::tuple<uint64_t, uint64_t> calculateMinorDiag(uint64_t piece){ //Don't have 
     if(piece & 0x8000000000000000ULL)
         return std::make_tuple(0x8000000000000000ULL, 0x8000000000000000ULL);
     else if(piece & 0x4080000000000000ULL)
-        return std::make_tuple(0x4000000000000000ULL, 0x0080000000000000ULL);
+        return std::make_tuple(0x0080000000000000ULL, 0x4000000000000000ULL);
     else if(piece & 0x2040800000000000ULL)
-        return std::make_tuple(0x2000000000000000ULL, 0x0000800000000000ULL);
+        return std::make_tuple(0x0000800000000000ULL, 0x2000000000000000ULL);
     else if(piece & 0x1020408000000000ULL)
-        return std::make_tuple(0x1000000000000000ULL, 0x0000008000000000ULL);
+        return std::make_tuple(0x0000008000000000ULL, 0x1000000000000000ULL);
     else if(piece & 0x0810204080000000ULL)
-        return std::make_tuple(0x0800000000000000ULL, 0x0000000080000000ULL);
+        return std::make_tuple(0x0000000080000000ULL, 0x0800000000000000ULL);
     else if(piece & 0x0408102040800000ULL)
-        return std::make_tuple(0x0400000000000000ULL, 0x0000000000800000ULL);
+        return std::make_tuple(0x0000000000800000ULL, 0x0400000000000000ULL);
     else if(piece & 0x0204081020408000ULL)
-        return std::make_tuple(0x0200000000000000ULL, 0x0000000000008000ULL);
+        return std::make_tuple(0x0000000000008000ULL, 0x0200000000000000ULL);
     else if(piece & 0x0102040810204080ULL)
-        return std::make_tuple(0x0100000000000000ULL, 0x0000000000000080ULL);
+        return std::make_tuple(0x0000000000000080ULL, 0x0100000000000000ULL);
     else if(piece & 0x0001020408102040ULL)
-        return std::make_tuple(0x0001000000000000ULL, 0x0000000000000040ULL);
+        return std::make_tuple(0x0000000000000040ULL, 0x0001000000000000ULL);
     else if(piece & 0x0000010204081020ULL)
-        return std::make_tuple(0x0000010000000000ULL, 0x0000000000000020ULL);  
+        return std::make_tuple(0x0000000000000020ULL, 0x0000010000000000ULL);  
     else if(piece & 0x0000000102040810ULL)
-        return std::make_tuple(0x0000000100000000ULL, 0x0000000000000010ULL); 
+        return std::make_tuple(0x0000000000000010ULL, 0x0000000100000000ULL); 
     else if(piece & 0x0000000001020408ULL)
-        return std::make_tuple(0x0000000001000000ULL, 0x0000000000000008ULL);   
+        return std::make_tuple(0x0000000000000008ULL, 0x0000000001000000ULL);   
     else if(piece & 0x0000000000010204ULL)
-        return std::make_tuple(0x0000000000010000ULL, 0x0000000000000004ULL);
+        return std::make_tuple(0x0000000000000004ULL, 0x0000000000010000ULL);
     else if(piece & 0x0000000000000102ULL)
-        return std::make_tuple(0x0000000000000100ULL, 0x0000000000000002ULL);
+        return std::make_tuple(0x0000000000000002ULL, 0x0000000000000100ULL);
     else
         return std::make_tuple(0x0000000000000001ULL, 0x0000000000000001ULL);         
+}
+
+std::tuple<uint64_t, uint64_t, uint64_t, uint64_t> generate_bishop(uint64_t bishop){
+    uint64_t upRight = 0;
+    uint64_t downRight = 0;
+    uint64_t downLeft = 0;
+    uint64_t upLeft = 0;
+    uint64_t tempBishop = bishop;
+
+    auto[minMain, maxMain] = calculateMainDiag(bishop);
+    auto[minMinor, maxMinor] = calculateMinorDiag(bishop);
+
+    while(tempBishop != maxMain){
+        tempBishop <<= 9;
+        upLeft++;
+    }
+    tempBishop = bishop;
+    while(tempBishop != minMain){
+        tempBishop >>= 9;
+        downRight++;
+    }
+    tempBishop = bishop;
+    while(tempBishop != maxMinor){
+        tempBishop <<= 7;
+        upRight++;
+    }
+    tempBishop = bishop;
+    while(tempBishop != minMinor){
+        tempBishop >>= 7;
+        downLeft++;
+    }
+    return std::make_tuple(upRight, downRight, downLeft, upLeft);
 }
 
 uint64_t move_bishop(uint64_t bishop, int direction, int n){
@@ -107,7 +137,9 @@ uint64_t move_bishop(uint64_t bishop, int direction, int n){
 }
 
 int main(){
-    uint64_t bishop = 0b0000000000000000000000000000000000000000000000000000000000000001;
-    print_board(0x0000000000000001ULL);
+    uint64_t bishop = 0x0000000800000000ULL;
+    print_board(bishop);
+    auto[upRight, downRight, downLeft, upLeft] = generate_bishop(bishop);
+    std::cout << "Up Right: " << upRight << "\nDown Right: " << downRight << "\nDown Left: " << downLeft << "\nUp Left: " << upLeft << std::endl;
     return 0;
 }
